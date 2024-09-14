98813ef653420b315e9ad2a4b5a5f565b971eef0
Andrei Bernatski <ABernatskiy@alfabank.ru>
2024-09-14 22:03:50 +0300

    fix: fix tags length validation bug

--- src/notes/schemas/fields.schema.ts
+++ src/notes/schemas/fields.schema.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export const idSchema = Joi.number().min(1).label('note id');
 
-export const tagSchema = Joi.string().min(1).label('tag');
+export const tagSchema = Joi.string().min(1).max(38).label('tag');
 
 export const colorSchema = Joi.string().min(3).label('color');
 
