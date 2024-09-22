9355573f784d61be620bc08088d32cd536f6e89f
Andrei Bernatski <ABernatskiy@alfabank.ru>
2024-09-22 16:58:13 +0300

    feat: add refresh endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { JwtService } from '@nestjs/jwt';
 import { UserModel } from '../../models/user.model';
 import { UserCredentials } from '../users/types';
 import * as bcrypt from 'bcrypt';
+import { UserDto } from '../users/dto/user.dto';
 
 @Injectable()
 export class AuthService {
@@ -18,7 +19,10 @@ export class AuthService {
 
   async signIn(userData: UserModel) {
     // Корректность пользовательских данных была проверена AuthLocalGuard (passport-local)
+
+    // TODO: получать пользователя из accessToken?
     const user = await this.usersServise.findOne(userData.email);
+
     const tokens = this.generateTokens(userData);
 
     return await this.usersServise.signIn({
@@ -64,9 +68,27 @@ export class AuthService {
     };
   }
 
+  async refresh(refreshToken: string) {
+    const user = await this.usersServise.findByRefreshToken(refreshToken);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const userData = new UserDto(user);
+
+    const tokens = this.generateTokens({
+      ...userData,
+    });
+
+    user.refreshToken = tokens.refreshToken;
+
+    return tokens;
+  }
+
   generateTokens(user: UserModel) {
     return {
-      accessToken: this.jwtService.sign(user, { expiresIn: '1d' }),
+      accessToken: this.jwtService.sign(user, { expiresIn: '30m' }),
       refreshToken: this.jwtService.sign(user, { expiresIn: '14d' }),
     };
   }
